92edf2a5e12dff58910393aa95f0ef6697df5aba
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-05 14:36:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return 'token 0fdff22a80a9729dc7e5c1028601c077982ff4c1';
+  return 'token e0336882be107fcf5fcd24a23ec734a46c8af72a';
 }
